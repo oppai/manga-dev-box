86543e6dcfab7fd744fafaaa4662a9f48a22331f
hiroaki.murayama <hiroaki.murayama@mixi.co.jp>
2015-03-03 14:16:39 +0900

    fix provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,8 +48,12 @@ install 'ExecJS runtime' nodejs
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
-install zsh tmux vim jq
-chsh -s `which zsh`
-git clone git@github.com:oppai/.dotfiles && cd ~/.dotfiles && sh ./setup.sh
+install zsh tmux tig vim jq
+chsh -s /bin/zsh vagrant
+su vagrant << EOF
+  if [ ! -f ~/.dotfiles ]; then
+    cd ~/ && git clone https://github.com/oppai/.dotfiles && cd ~/.dotfiles && sh ./setup.sh
+  fi
+EOF
 
 echo 'all set, rock on!'
