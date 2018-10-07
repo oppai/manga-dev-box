4870ca99316341c368d3d53977eb725366084001
oppai <hotsoup.h@gmail.com>
2018-10-07 20:12:38 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,4 +45,8 @@ install JQ jq
 #  fi
 #EOF
 
+# install chrome
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+
 echo 'all set, rock on!'
